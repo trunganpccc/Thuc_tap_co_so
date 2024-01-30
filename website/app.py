5a8f516ae0334b78c1d7da40530d177c4a215ade
trunganpccc <n19dccn003@student.ptithcm.edu.vn>
2024-01-30 22:54:12 +0700

    push the code to github

--- website/app.py
+++ website/app.py
@@ -16,6 +16,39 @@ def login():
     if 'username' in session:
         return redirect(url_for('home'))
     return render_template('login.html')
+# Xử lý đăng nhập
+@app.route('/login', methods=['GET', 'POST'])
+def do_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        router = graph.nodes.match("Router", username=username, password=password).first()
+        if router:
+            # Lưu thông tin người dùng vào session
+            session['username'] = username
+            return redirect(url_for('home'))
+        else:
+            return render_template('login_fail.html')
+    
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    if 'username' in session:
+        username = session['username']
+        info_wifi = []
+        query = """
+        MATCH (w:Wifi)-[:Locatted_at]->(c:Class)
+        RETURN c.name AS name, w.ssid AS ssid, w.password AS password, w.ip_address AS ip_address, w.mac_address AS mac_address, w.speed AS speed
+
+        """
+        result = graph.run(query)
+        for record in result:
+            info_wifi.append(dict(record))
+        return render_template('home.html',username=username, info_wifi=info_wifi)
+    else:
+        return redirect(url_for('login'))
 # Đăng xuất
 @app.route('/logout')
 def logout():
